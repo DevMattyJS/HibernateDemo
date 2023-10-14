3548a356071687fef783b6c3514a0c586421430a
DevMattyJS <matej.sliacky@protonmail.com>
2023-10-14 20:00:33 +0200

    Added basic configuration for database connection, also added CommandLineRunner boiler-plate code

--- src/main/java/com/msc/cruddemo/CruddemoApplication.java
+++ src/main/java/com/msc/cruddemo/CruddemoApplication.java
@@ -1,13 +1,24 @@
 package com.msc.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	// CommandLineRunner - from a SpringBoot framework
+	// This code will be executed after the spring beans have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello world");
+		};
 	}
 
 }
