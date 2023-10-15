de1aa0ce8b9583f400ff3296fb64e47f5227e541
DevMattyJS <matej.sliacky@protonmail.com>
2023-10-15 10:15:37 +0200

    Implemented feature to create new Student object and save it to database

--- src/main/java/com/msc/cruddemo/CruddemoApplication.java
+++ src/main/java/com/msc/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.msc.cruddemo;
 
+import com.msc.cruddemo.dao.StudentDAO;
+import com.msc.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +17,24 @@ public class CruddemoApplication {
 	// CommandLineRunner - from a SpringBoot framework
 	// This code will be executed after the spring beans have been loaded
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello world");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create new student object
+		System.out.println("Creating new student ...");
+		Student tempStudent = new Student("John", "Doe", "jd@email.com");
+
+		// save student object
+		System.out.println("Saving student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of saved student
+		System.out.println("Student saved. Generated id: " + tempStudent.getId());
 	}
 
 }
